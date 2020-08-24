eee761b64f76bdf5ae73633393f4782579070039
Christopher Peters <cpeter9@gmail.com>
2020-08-24 19:32:36 +0000

    Apparently Rcpp now prepends underscores to function names.

--- src/init.c
+++ src/init.c
@@ -141,7 +141,7 @@ extern SEXP cdfit_gaussian_hsr_bedpp_nac(SEXP X_, SEXP y_, SEXP row_idx_,
                                              SEXP safe_thresh_,
                                              SEXP verbose_);
 
-extern SEXP biglasso_get_eta(SEXP xPSEXP, SEXP row_idx_SEXP, SEXP betaSEXP, SEXP idx_pSEXP, SEXP idx_lSEXP);
+extern SEXP _biglasso_get_eta(SEXP xPSEXP, SEXP row_idx_SEXP, SEXP betaSEXP, SEXP idx_pSEXP, SEXP idx_lSEXP);
 
 static R_CallMethodDef callMethods[] = {
   {"cdfit_binomial_hsr", (DL_FUNC) &cdfit_binomial_hsr, 16},
@@ -162,7 +162,7 @@ static R_CallMethodDef callMethods[] = {
   {"cdfit_gaussian_edpp", (DL_FUNC) &cdfit_gaussian_edpp, 14},
   {"cdfit_gaussian_hsr_dome_nac", (DL_FUNC) &cdfit_gaussian_hsr_dome_nac, 16},
   {"cdfit_gaussian_hsr_bedpp_nac", (DL_FUNC) &cdfit_gaussian_hsr_bedpp_nac, 16},
-  {"biglasso_get_eta", (DL_FUNC) &biglasso_get_eta, 5},
+  {"_biglasso_get_eta", (DL_FUNC) &_biglasso_get_eta, 5},
   {NULL, NULL, 0}
 };
 
